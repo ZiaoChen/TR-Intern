6b77904c1ce841ebb4b1cd30fed23f6dfd8f7793
ZiaoChen <466930995@qq.com>
2018-04-10 17:51:38 +0800

    update

--- Offshore Score/add_new_paradise_paper.py
+++ Offshore Score/add_new_paradise_paper.py
@@ -51,50 +51,50 @@ session = driver.session()
 # print(list(r))
 # print("All officer nodes were inserted")
 #
-# Insert Entity Nodes
-a = session.run("""
-USING PERIODIC COMMIT 500
-LOAD CSV WITH HEADERS FROM 'https://drive.google.com/uc?export=download&id=1H7K2e4OaevLB1R2fn3on3utOPyBV1mIA' AS csvLine
-with csvLine
-where csvLine.sourceID <> 'Paradise Papers - Appleby'
-create (p:Entity:New { node_id: csvLine.node_id, name: csvLine.name})
-set
-p.jurisdiction = toString(csvLine.jurisdiction),
-p.jurisdiction_description = toString(csvLine.jurisdiction_description),
-p.sourceID = toString(csvLine.sourceID),
-p.country_codes = toString(csvLine.country_codes),
-p.countries = toString(csvLine.countries),
-p.incorporation_date = toString(csvLine.incorporation_date),
-p.inactive_date = toString(csvLine.inactive_date),
-p.struck_off_date = toString(csvLine.struck_off_date),
-p.closed_date = toString(csvLine.closed_date),
-p.ibcRUC = toString(csvLine.ibcRUC),
-p.company_type = toString(csvLine.company_type),
-p.service_provider = toString(csvLine.service_provider),
-p.status = toString(csvLine.status),
-p.valid_until = toString(csvLine.valid_until),
-p.note = toString(csvLine.note)""")
-print(list(a))
-print("All entity nodes were inserted")
-
-# Create index
-a = session.run("""
-create index on :New(node_id)
-""")
-
+# # Insert Entity Nodes
 # a = session.run("""
-# create constraint on (g:Entity)
-# assert g.node_id is UNIQUE
-# """)
-# print("Index on entity node id is created")
+# USING PERIODIC COMMIT 500
+# LOAD CSV WITH HEADERS FROM 'https://drive.google.com/uc?export=download&id=1H7K2e4OaevLB1R2fn3on3utOPyBV1mIA' AS csvLine
+# with csvLine
+# where csvLine.sourceID <> 'Paradise Papers - Appleby'
+# create (p:Entity:New { node_id: csvLine.node_id, name: csvLine.name})
+# set
+# p.jurisdiction = toString(csvLine.jurisdiction),
+# p.jurisdiction_description = toString(csvLine.jurisdiction_description),
+# p.sourceID = toString(csvLine.sourceID),
+# p.country_codes = toString(csvLine.country_codes),
+# p.countries = toString(csvLine.countries),
+# p.incorporation_date = toString(csvLine.incorporation_date),
+# p.inactive_date = toString(csvLine.inactive_date),
+# p.struck_off_date = toString(csvLine.struck_off_date),
+# p.closed_date = toString(csvLine.closed_date),
+# p.ibcRUC = toString(csvLine.ibcRUC),
+# p.company_type = toString(csvLine.company_type),
+# p.service_provider = toString(csvLine.service_provider),
+# p.status = toString(csvLine.status),
+# p.valid_until = toString(csvLine.valid_until),
+# p.note = toString(csvLine.note)""")
+# print(list(a))
+# print("All entity nodes were inserted")
 #
+# # Create index
 # a = session.run("""
-# create index on :Officer(node_id)
+# create index on :New(node_id)
 # """)
-
-# a = session.run("""
-# create constraint on (g:Officer)
-# assert g.node_id is UNIQUE """)
+#
+# # a = session.run("""
+# # create constraint on (g:Entity)
+# # assert g.node_id is UNIQUE
+# # """)
+# # print("Index on entity node id is created")
+# #
+# # a = session.run("""
+# # create index on :Officer(node_id)
+# # """)
+#
+# # a = session.run("""
+# # create constraint on (g:Officer)
+# # assert g.node_id is UNIQUE """)
 
 print("Index on node id is created")
 
@@ -111,7 +111,7 @@ match (to:New{node_id: toString(csvLine.END_ID)})
 using index to:New(node_id)
 with from, to, csvLine
 where from.sourceID <> 'Paradise Papers - Appleby' and to.sourceID <> 'Paradise Papers - Appleby' and csvLine.TYPE = 'officer_of'
-create (from)-[r:connect]->(to)
+create (from)-[r:isOfficerOfNew]->(to)
 set
 r.start_date = toString(csvLine.start_date),
 r.end_date = toString(csvLine.end_date),
